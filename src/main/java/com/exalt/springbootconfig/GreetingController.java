d554994063405c4aecd7a9561858209f92d08a2b
Laith Budairi <LaithB@exalt.ps>
2021-01-18 11:54:13 +0200

    Initial Commit

---
+++ src/main/java/com/exalt/springbootconfig/GreetingController.java
@@ -0,0 +1,43 @@
+package com.exalt.springbootconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GreetingController {
+
+    @Value("${my.greeting: default value}")
+    private String greetingMessage;
+
+    @Value("static message")
+    private String staticMessage;
+
+    @Value("${my.list.value}")
+    private List<String> listValues;
+
+    @Autowired
+    private Environment env;
+
+//    @Value("#{${dbValues}}")
+//    private Map<String, String> dbValues;
+
+    @Autowired
+    private DBSettings dbSettings;
+
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return dbSettings.getConnection() + dbSettings.getHost();
+    }
+
+    @GetMapping("/envdetails")
+    public String envdetails() {
+        return env.toString();
+    }
+}
